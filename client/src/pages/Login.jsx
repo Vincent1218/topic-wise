49b61ff6f4c1c3018e976c40ac36cf97a6d6e1e9
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-07-26 17:22:12 +0530

    style added tailwind ui components to /signup and /login
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 export default function Login() {
   const navigate = useNavigate();
-  const [serverRes, setServerRes] = useState(" ");
+  // const [serverRes, setServerRes] = useState(" ");
   const handleError = (err) =>
     toast.error(err, {
       position: "bottom-left",
@@ -43,50 +43,130 @@ export default function Login() {
     }
   }
   return (
-    <div>
-      <h1>Login </h1>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          signup(values.email, values.password);
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-      <p>
-        If you don&apos;t have an account already{" "}
-        <Link to="/signup">Sign up</Link>
-      </p>
-      <div>
-        {serverRes !== " " ? (
-          JSON.stringify(serverRes)
-        ) : (
-          <p>User not logged in</p>
-        )}
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          {/* <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
+            Or{" "}
+            <Link
+              to="/signup"
+              className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+            >
+              start your 14-day free trial
+            </Link>
+          </p> */}
+        </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            signup(values.email, values.password);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="mt-8">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm">
+                <div>
+                  <Field
+                    aria-label="Email address"
+                    name="email"
+                    type="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div className="-mt-px">
+                  <Field
+                    aria-label="Password"
+                    name="password"
+                    type="password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              <div className="mt-6 flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember_me"
+                    type="checkbox"
+                    className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                  />
+                  <label
+                    htmlFor="remember_me"
+                    className="ml-2 block text-sm leading-5 text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <div className="text-sm leading-5">
+                  <a
+                    href="#"
+                    className="font-medium text-slate-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                  >
+                    Don&apos;t have an account?{" "}
+                    <Link className="text-indigo-500" to="/signup">
+                      Sign up
+                    </Link>{" "}
+                  </a>
+                </div>
+              </div>
+              <div className="mt-6">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <svg
+                      className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5 9a7 7 0 1114 0 7 7 0 01-14 0zm7-6a6 6 0 100 12A6 6 0 0012 3zm.707 3.293a1 1 0 00-1.414 0L9 7.586 7.707 6.293a1 1 0 00-1.414 1.414L7.586 9l-1.293 1.293a1 1 0 101.414 1.414L9 10.414l1.293 1.293a1 1 0 001.414-1.414L10.414 9l1.293-1.293a1 1 0 000-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                  Sign in
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+        {/* <div>
+          {serverRes !== " " ? (
+            JSON.stringify(serverRes)
+          ) : (
+            <p>User not logged in</p>
+          )}
+        </div> */}
+        <ToastContainer />
       </div>
-      <ToastContainer />
     </div>
   );
 }
