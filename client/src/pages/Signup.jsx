db26ea0d25e563d3f26133ff53d3a24067a87b5e
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-07-26 18:04:56 +0530

    bug changed redirect from signup to login && feat changed forgot password input to show password
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,11 @@
 import { Formik, Form, Field } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
 export default function Signup() {
   const navigate = useNavigate();
 
+  const [showPassword, setShowPassword] = useState(false);
   const handleError = (err) =>
     toast.error(err, {
       position: "bottom-left",
@@ -31,7 +33,7 @@ export default function Signup() {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-          navigate("/");
+          navigate("/login");
         }, 1000);
       } else {
         handleError(message);
@@ -41,15 +43,15 @@ export default function Signup() {
     }
   }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full">
+    <div className="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-50 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md">
         <div>
           <img
-            className="mx-auto h-12 w-auto"
+            className="w-auto h-12 mx-auto"
             src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
             alt="Workflow"
           />
-          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+          <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
             Sign up for an account
           </h2>
         </div>
@@ -81,7 +83,7 @@ export default function Signup() {
                     name="email"
                     type="email"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                    className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
                     placeholder="Email address"
                   />
                 </div>
@@ -89,30 +91,31 @@ export default function Signup() {
                   <Field
                     aria-label="Password"
                     name="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                    className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
                     placeholder="Password"
                   />
                 </div>
-                <div className="mt-6 flex items-center justify-between">
+                <div className="flex items-center justify-between mt-6">
                   <div className="flex items-center">
                     <input
                       id="remember_me"
                       type="checkbox"
-                      className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                      className="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox"
+                      onChange={() => setShowPassword(!showPassword)}
                     />
                     <label
                       htmlFor="remember_me"
-                      className="ml-2 block text-sm leading-5 text-gray-900"
+                      className="block ml-2 text-sm leading-5 text-gray-900"
                     >
-                      Remember me
+                      Show Password
                     </label>
                   </div>
                   <div className="text-sm leading-5">
                     <a
                       href="#"
-                      className="font-medium text-slate-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                      className="font-medium transition duration-150 ease-in-out text-slate-600 hover:text-indigo-500 focus:outline-none focus:underline"
                     >
                       Already have an account?{" "}
                       <Link
@@ -130,11 +133,11 @@ export default function Signup() {
                 <button
                   type="submit"
                   disabled={isSubmitting}
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  className="relative flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
                 >
-                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                     <svg
-                      className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                      className="w-5 h-5 text-indigo-500 transition duration-150 ease-in-out group-hover:text-indigo-400"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
