894e1c27e094e8774bd9d597ae77d42fd0dcbb1c
unknown <yajatgulati01@gmail.com>
2023-07-27 19:01:30 +0530

    feat saving score functionality added /classify route
    Signed-off-by: unknown <yajatgulati01@gmail.com>

--- server/routes/api/classify.js
+++ server/routes/api/classify.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli";
-const headers = {"Authorization": "Bearer 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"};
+const axios = require("axios");
+
+const { User } = require("./../../models");
+const API_URL =
+  "https://api-inference.huggingface.co/models/facebook/bart-large-mnli";
+const headers = {
+  Authorization: "Bearer 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",
+};
 
 const disciplines = ["technology", "business", "science", "humanities"];
 const disciplineCounts = {};
-disciplines.forEach(discipline => disciplineCounts[discipline] = 0);
+disciplines.forEach((discipline) => (disciplineCounts[discipline] = 0));
 
 const query = async (sentence) => {
-  const response = await axios.post(API_URL, 
-    { "inputs": sentence, "parameters": {"candidate_labels": disciplines} },
+  const response = await axios.post(
+    API_URL,
+    { inputs: sentence, parameters: { candidate_labels: disciplines } },
     { headers }
   );
   return response.data;
-}
+};
 
 router.post("/", async (req, res) => {
-  const { content } = req.body;
+  const { postId, userId, content } = req.body;
   // assuming paragraphs are separated by two newline characters
-  const paragraphs = content.split('\n\n');
+  const paragraphs = content.split("\n\n");
   // array to store disciplines in each paragraph
   const paragraphDisciplines = [];
   let totalSentences = 0;
 
-  for(const paragraph of paragraphs) {
+  for (const paragraph of paragraphs) {
     const sentences = paragraph.split(/(?<=[.!?])\s+(?=[A-Z])/);
     // keeping track of disciplines in current paragraph
     const currentParagraphDisciplines = new Set();
@@ -47,14 +53,23 @@ router.post("/", async (req, res) => {
 
   // calculating percentage of each discipline
   const percentages = {};
-  for(const discipline in disciplineCounts) {
-    percentages[discipline] = ((disciplineCounts[discipline] / totalSentences) * 100).toFixed(2);
+  for (const discipline in disciplineCounts) {
+    percentages[discipline] = (
+      (disciplineCounts[discipline] / totalSentences) *
+      100
+    ).toFixed(2);
   }
-  const DGI = (Object.keys(disciplineCounts).length / paragraphs.length).toFixed(2);
+  const DGI = (
+    Object.keys(disciplineCounts).length / paragraphs.length
+  ).toFixed(2);
 
   //calculating DII
-  const multiDisciplinaryParagraphs = paragraphDisciplines.filter(disciplines => disciplines.size > 1);
-  const DII = (multiDisciplinaryParagraphs.length / paragraphs.length).toFixed(2);
+  const multiDisciplinaryParagraphs = paragraphDisciplines.filter(
+    (disciplines) => disciplines.size > 1
+  );
+  const DII = (multiDisciplinaryParagraphs.length / paragraphs.length).toFixed(
+    2
+  );
 
   let sumDisciplines = 0;
   for (const discipline in disciplineCounts) {
@@ -67,30 +82,47 @@ router.post("/", async (req, res) => {
     DEI += fraction ** 2;
   }
   DEI = (1 - DEI).toFixed(2);
-  
+
   const responseMetrics = {
-    "essayMetrics": {
-      "numParagraphs": paragraphs.length,
-      "DGI": DGI,
-      "DII": DII,
-      "DEI": DEI
+    essayMetrics: {
+      numParagraphs: paragraphs.length,
+      DGI: DGI,
+      DII: DII,
+      DEI: DEI,
     },
-    "paragraphMetrics": {
-      "numMultiDisciplinaryParagraphs": multiDisciplinaryParagraphs.length
+    paragraphMetrics: {
+      numMultiDisciplinaryParagraphs: multiDisciplinaryParagraphs.length,
     },
-    "disciplineMetrics": {
-      "total": sumDisciplines
-    }
+    disciplineMetrics: {
+      total: sumDisciplines,
+    },
   };
 
   disciplines.forEach((discipline) => {
     responseMetrics["disciplineMetrics"][discipline] = {
-      "count": disciplineCounts[discipline],
-      "percentage": percentages[discipline]
+      count: disciplineCounts[discipline],
+      percentage: percentages[discipline],
     };
   });
+  //updating backend
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
 
-  res.json(responseMetrics);
+  const post = user.posts.id(postId);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+
+  // Append the scores to the post
+  post.scores = responseMetrics;
+
+  // Save the changes made to the user document
+  await user.save();
+
+  // Return a success response
+  res.json({ message: "Scores saved successfully." });
 });
 
 module.exports = router;
